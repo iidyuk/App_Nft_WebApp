e165d274a748c13056815896b03ed5360e617c66
iyda_loox <signup4320@gmail.com>
2025-09-20 16:57:55 +0900

    edit list page を作成

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,7 +5,9 @@
     <div class="space-y-4">
       <NuxtLink to="/about" class="inline-block bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-6 rounded transition">Registration</NuxtLink>
       <br>
-      <NuxtLink to="/pinata-test" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded transition">Pinata API Test</NuxtLink>
+      <NuxtLink to="/list" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded transition">List</NuxtLink>
+      <br>
+      <NuxtLink to="/pinata-test" class="inline-block bg-gray-500 hover:bg-gray-600 text-white font-semibold py-2 px-6 rounded transition">Pinata API Test</NuxtLink>
     </div>
   </div>
 </template>
---
+++ src/pages/list/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="max-w-6xl mx-auto mt-20 p-8 bg-white rounded-lg shadow">
+    <h1 class="text-3xl font-bold text-blue-600 mb-4 text-center">List</h1>
+
+    <NuxtLink to="/" class="inline-block bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-6 rounded transition mt-8">Top Page</NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
