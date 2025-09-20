b14471996cfb90a0f0f074a95845e2ca31872fb1
iyda_loox <signup4320@gmail.com>
2025-09-20 16:07:13 +0900

    update RegistrationページCSS

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="max-w-lg mx-auto mt-20 p-8 bg-white rounded-lg shadow text-center">
-    <h1 class="text-3xl font-bold text-green-600 mb-4">トップページへようこそ！</h1>
-    <p class="mb-6 text-gray-700">ここに好きな内容を書けます。</p>
+    <h1 class="text-3xl font-bold text-green-600 mb-4">nft</h1>
+    <p class="mb-6 text-gray-700">Let's make an NFT from this jpg/jpeg file.</p>
     <div class="space-y-4">
-      <NuxtLink to="/about" class="inline-block bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-6 rounded transition">About ページへ</NuxtLink>
+      <NuxtLink to="/about" class="inline-block bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-6 rounded transition">Registration</NuxtLink>
       <br>
-      <NuxtLink to="/pinata-test" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded transition">Pinata APIテスト</NuxtLink>
+      <NuxtLink to="/pinata-test" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded transition">Pinata API Test</NuxtLink>
     </div>
   </div>
 </template>
